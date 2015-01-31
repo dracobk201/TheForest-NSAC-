ce6f4101eab3674b9e3da1b4fa2bb3f486701a2a
Erick Subero <dracobk201@gmail.com>
2015-01-31 14:24:02 -0430

    First Commit
    cha-chaaaan

---
+++ Assets/Scripts/scrPlayerSkills.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scrPlayerSkills : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (GameMaster.instance.varEspaciosDisponibles == 0) {
+				GameMaster.instance.varEnergiaDisponible++;
+
+				//Debug.Log("Procedo a consumir el objeto");
+				GameMaster.instance.goTextoDescrip.GetComponent<Text>().text = "Procedo a consumir el objeto";
+				if (GameMaster.instance.varEnergiaDisponible > 6) {
+					GameMaster.instance.varEnergiaDisponible = 6;
+				}
+				GameMaster.instance.varEspaciosDisponibles++;
+				GameMaster.instance.goTextoEnergia.GetComponent<Text>().text = "Energia = " + GameMaster.instance.varEnergiaDisponible;
+				GameMaster.instance.goTextoObjetos.GetComponent<Text>().text = "Disponible = " + GameMaster.instance.varEspaciosDisponibles;
+			} else {
+				//Debug.Log("No tienes un objeto a la mano");
+				GameMaster.instance.goTextoDescrip.GetComponent<Text>().text = "No tienes un objeto a la mano";
+			}
+		}
+	}
+}
